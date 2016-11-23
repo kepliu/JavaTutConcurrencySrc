619d43a7815afd5bd0086a65cde80290b3a4b658
kepliu <kepliu@gmail.com>
2016-11-23 11:19:46 -0500

    Update HelloRunnable.java

--- javaTut/HelloRunnable.java
+++ javaTut/HelloRunnable.java
@@ -3,7 +3,7 @@ package javaTut;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HelloRunnable implements Runnable {
+public class HelloRunnable implements Runnable {//test git
 	public static List<String> list = new ArrayList<String>();
 	private String name;
 	private int n;
